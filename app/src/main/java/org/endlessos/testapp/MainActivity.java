33f0814b44f202905750f2c1cd62ecc9aef6be6f
Dan Nicholson <dbn@endlessos.org>
2023-07-19 17:03:30 -0600

    try to run kolibri

--- app/src/main/java/org/endlessos/testapp/MainActivity.java
+++ app/src/main/java/org/endlessos/testapp/MainActivity.java
@@ -1,5 +1,7 @@
 package org.endlessos.testapp;
 
+import java.io.File;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,17 +13,48 @@ import com.chaquo.python.Python;
 public class MainActivity extends Activity {
     private static final String TAG = "MainActivity";
 
+    private Python python;
+    private PyObject mainModule;
+    private Thread serverThread;
+    private String serverUrl;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Log.i(TAG, "Creating activity");
+
         WebView view = new WebView(this);
         setContentView(view);
-        view.loadUrl("https://google.com");
 
-        Python python = Python.getInstance();
-        PyObject main = python.getModule("main");
-        PyObject out = main.callAttr("test", "test");
-        Log.i(TAG, out.toString());
+        python = Python.getInstance();
+        mainModule = python.getModule("main");
+
+        File kolibriHome = new File(this.getFilesDir(), "kolibri");
+        mainModule.callAttr("setup", kolibriHome.toString());
+
+        mainModule.callAttr("get_url").toString();
+
+        serverThread = new Thread(new ServerThread(), "ServerThread");
+        serverThread.start();
+
+        serverUrl = mainModule.callAttr("get_url").toString();
+        view.loadUrl(serverUrl);
+    }
+
+    class ServerThread implements Runnable {
+        @Override
+        public void run() {
+            Log.i(TAG, "Starting Kolibri server");
+            mainModule.callAttr("start");
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "Destroying activity");
+        Log.i(TAG, "Stopping Kolibri server");
+        mainModule.callAttr("stop");
     }
 }
--- app/src/main/python/main.py
+++ app/src/main/python/main.py
@@ -1,2 +1,32 @@
-def test(msg):
-    return f'received "{msg}"'
+import logging
+import os
+
+logger = logging.getLogger(__name__)
+
+
+def setup(kolibri_home):
+    os.environ['KOLIBRI_HOME'] = kolibri_home
+    os.environ['KOLIBRI_DEPLOYMENT_LISTEN_ADDRESS'] = '127.0.0.1'
+
+    from kolibri.utils import env
+    env.set_env()
+    import kolibri
+    finder = kolibri.__loader__.finder
+    finder.extract_dir("kolibri/dist")
+    from kolibri.utils import server
+
+
+def start():
+    from kolibri.utils import server
+    server.start()
+
+
+def stop():
+    from kolibri.utils import server
+    server.stop()
+
+
+def get_url():
+    from kolibri.utils import server
+    _, _, port = server.get_status()
+    return f'http://127.0.0.1:{port}/'
