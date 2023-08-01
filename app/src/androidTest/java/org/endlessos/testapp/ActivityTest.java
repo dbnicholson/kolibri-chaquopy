c693b2d208d1e085b36204a445653c0aae94c76a
Dan Nicholson <dbn@endlessos.org>
2023-08-01 15:40:07 -0600

    Comment out 2nd launch test for now

--- app/src/androidTest/java/org/endlessos/testapp/ActivityTest.java
+++ app/src/androidTest/java/org/endlessos/testapp/ActivityTest.java
@@ -1,5 +1,6 @@
 package org.endlessos.testapp;
 
+import android.util.Log;
 import androidx.lifecycle.Lifecycle.State;
 import androidx.test.core.app.ActivityScenario;
 import org.junit.Test;
@@ -7,6 +8,8 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class ActivityTest {
+    private static final String TAG = "ActivityTest";
+
     @Test
     public void testLaunch() {
         try(ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class)) {
@@ -14,13 +17,17 @@ public class ActivityTest {
         }
     }
 
-    @Test
-    public void testStates() {
-        try(ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class)) {
-            scenario.moveToState(State.RESUMED);
-            scenario.moveToState(State.STARTED);
-            scenario.moveToState(State.CREATED);
-            scenario.moveToState(State.DESTROYED);
-        }
-    }
+    // @Test
+    // public void testStates() {
+    //     try(ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class)) {
+    //         Log.i(TAG, "Moving to RESUMED, current state is " + scenario.getState());
+    //         scenario.moveToState(State.RESUMED);
+    //         Log.i(TAG, "Moving to STARTED, current state is " + scenario.getState());
+    //         scenario.moveToState(State.STARTED);
+    //         Log.i(TAG, "Moving to CREATED, current state is " + scenario.getState());
+    //         scenario.moveToState(State.CREATED);
+    //         Log.i(TAG, "Moving to DESTROYED, current state is " + scenario.getState());
+    //         scenario.moveToState(State.DESTROYED);
+    //     }
+    // }
 }
