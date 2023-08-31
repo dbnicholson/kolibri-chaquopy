1e5f4960905c9176444c93474aff9bd7bf2b9218
Dan Nicholson <dbn@endlessos.org>
2023-08-31 15:25:02 -0600

    Add authentication middleware

--- app/src/main/python/testapp/settings.py
+++ app/src/main/python/testapp/settings.py
@@ -2,3 +2,8 @@ from kolibri.deployment.default.settings.base import *  # noqa E402
 
 # Don't configure any logging in Django since we already did it.
 LOGGING_CONFIG = None
+
+# Add our authentication middleware.
+MIDDLEWARE.append(
+    "testapp.middleware.AlwaysAuthenticatedMiddleware"
+)
