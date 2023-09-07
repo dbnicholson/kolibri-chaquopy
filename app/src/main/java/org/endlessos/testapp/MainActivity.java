fa5e0d632d2774c564563504eae19412ddea9fce
Dan Nicholson <dbn@endlessos.org>
2023-09-07 16:15:49 -0600

    Fix up some lint issues
    And show deprecation warnings.

--- app/src/main/java/org/endlessos/testapp/MainActivity.java
+++ app/src/main/java/org/endlessos/testapp/MainActivity.java
@@ -1,5 +1,6 @@
 package org.endlessos.testapp;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
@@ -21,6 +22,7 @@ public class MainActivity extends Activity {
     private String serverUrl;
 
     @Override
+    @SuppressLint("SetJavaScriptEnabled")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
