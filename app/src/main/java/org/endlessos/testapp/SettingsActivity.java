7419a62cac1bae5d5c0aeb0fdc9272a153d68dac
Dylan McCall <dylan@dylanmccall.ca>
2023-10-06 15:09:23 -0700

    WIP

--- app/src/main/java/org/endlessos/testapp/SettingsActivity.java
+++ app/src/main/java/org/endlessos/testapp/SettingsActivity.java
@@ -7,28 +7,32 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toolbar;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipException;
+import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
 
 public class SettingsActivity extends Activity {
-    // TODO: Add a way back to the main activity.
-    // TODO: Restart the Kolibri service if it is running.
-    // TODO: Show progress while importing.
-    // TODO: Disable the import button while importing.
-    // TODO: Display helpful information after importing.
+    // TODO: Restart the Kolibri service if it is running?
+    // TODO: Display helpful information after choosing a collection, such as its name from manifest.json
 
     private static final String TAG = Constants.TAG;
 
     private static final int CHOOSE_FILE_RESULT_CODE = 8778;
+
+    Button chooseContentButton;
+    Button importContentButton;
+    ProgressBar importContentProgress;
+    TextView outputTextView;
+
+    private Uri selectedContentUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,20 +41,34 @@ public class SettingsActivity extends Activity {
 
         getActionBar().setTitle("Endless Key Settings");
 
-        final Button importContentButton = findViewById(R.id.importContentButton);
+        chooseContentButton = findViewById(R.id.chooseContentButton);
+        chooseContentButton.setOnClickListener(
+                new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View view) {
+                        startChooseContent();
+                    }
+                }
+        );
+
+        importContentButton = findViewById(R.id.importContentButton);
         importContentButton.setOnClickListener(
                 new View.OnClickListener() {
 
                     @Override
                     public void onClick(View view) {
-                        Log.i("importContent", "onClick");
                         startImportContent();
                     }
                 }
         );
+
+        importContentProgress = findViewById(R.id.importContentProgress);
+
+        outputTextView = findViewById(R.id.outputTextView);
     }
 
-    private void startImportContent() {
+    private void startChooseContent() {
         Intent chooseFile = new Intent(Intent.ACTION_GET_CONTENT);
         chooseFile.addCategory(Intent.CATEGORY_OPENABLE);
         chooseFile.setType("application/zip");
@@ -58,6 +76,40 @@ public class SettingsActivity extends Activity {
                 Intent.createChooser(chooseFile, "Choose a file"),
                 CHOOSE_FILE_RESULT_CODE
         );
+    }
+
+    private void startImportContent() {
+        chooseContentButton.setEnabled(false);
+        importContentButton.setEnabled(false);
+        importContentProgress.setIndeterminate(true);
+
+        try {
+            outputTextView.setText("Importing...");
+            copyKolibriContent(
+                    selectedContentUri,
+                    KolibriUtils.getKolibriHome(getBaseContext())
+            );
+            setSelectedContentUri(null);
+        } catch (IOException e) {
+            outputTextView.setText("Import error:\n"+e.getStackTrace().toString());
+            return;
+        } finally {
+            chooseContentButton.setEnabled(true);
+            importContentProgress.setIndeterminate(false);
+        }
+
+        outputTextView.setText("Finished importing");
+    }
+
+    private void setSelectedContentUri(Uri value) {
+        selectedContentUri = value;
+        if (value != null) {
+            importContentButton.setEnabled(true);
+            outputTextView.setText("Ready to import '" + selectedContentUri.toString() + "'");
+        } else {
+            importContentButton.setEnabled(false);
+            outputTextView.setText("");
+        }
     }
 
     @Override
@@ -72,21 +124,15 @@ public class SettingsActivity extends Activity {
             return;
         }
 
-        try {
-            copyKolibriContent(
-                    data.getData(),
-                    KolibriUtils.getKolibriHome(getBaseContext())
-            );
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        setSelectedContentUri(data.getData());
     }
 
     protected void copyKolibriContent(Uri sourceContent, File outputDirectory) throws IOException {
+        // TODO: This needs to happen asynchronously.
+
         InputStream fileInput;
 
         fileInput = getContentResolver().openInputStream(sourceContent);
-
         ZipInputStream zipInput = new ZipInputStream(new BufferedInputStream(fileInput));
         ZipEntry zipEntry = null;
         byte[] buffer = new byte[1024];
